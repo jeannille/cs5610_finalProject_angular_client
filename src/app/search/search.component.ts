c6534a95497820de468a58d0ae096986a604fa8c
zhuje <59589720+zhuje@users.noreply.github.com>
2020-12-01 21:46:34 -0500

    changed routing in search component to route to search/:searchText

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {OMDBServiceClient} from '../services/OMDBServiceClient';
 import {ActivatedRoute} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {error} from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-search',
@@ -14,17 +16,18 @@ export class SearchComponent implements OnInit {
   };
   urlSearchText = '';
 
+
+
   constructor(private omdbService: OMDBServiceClient,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    // this.route.params.subscribe(params => {
-    //   this.urlSearchText = params.searchText;
-    //   if (this.searchText !== '') {
-    //     this.omdbService.fetchMoviesBySearchText(this.urlSearchText)
-    //       .then(results => this.moviesSearchResult = results);
-    //   }
-    // });
+    this.route.params.subscribe(params => {
+      this.urlSearchText = params.searchText;
+      if (this.urlSearchText !== undefined) {
+        this.fetchMoviesBySearchText(this.urlSearchText);
+      }
+    });
   }
 
   fetchMoviesBySearchText = (searchText) => {
